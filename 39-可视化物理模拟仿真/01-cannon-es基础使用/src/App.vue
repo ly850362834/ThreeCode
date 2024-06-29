2e53d397ee479def7fd530c3838c383f29d95022
刘宇 <850362834@qq.com>
2024-06-29 16:48:18 +0800

    平面球的碰撞

--- 39-可视化物理模拟仿真/01-cannon-es基础使用/src/App.vue
+++ 39-可视化物理模拟仿真/01-cannon-es基础使用/src/App.vue
@@ -15,6 +15,7 @@ world.gravity.set(0, -9.82, 0);
 // 初始化3D场景
 const scene = new THREE.Scene();
 // 初始化相机
+
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -34,66 +35,71 @@ controls.enableDamping = true;
 
 // 创建一个球体
 const sphereShape = new CANNON.Sphere(0.5);
-// 创建一个刚体
+
+//创建一个刚体
 const sphereBody = new CANNON.Body({
-  mass: 1,
-  shape: sphereShape,
-  position: new CANNON.Vec3(0, 5, 0),
-});
-// 将刚体添加到物理世界
+  //重量
+  mass:1,
+  //形状
+  shape:sphereShape,
+  //位置
+  position:new CANNON.Vec3(0,5,0)
+})
+//将刚体添加到物理世界
 world.addBody(sphereBody);
 
-// 创建一个物理世界的平面
-// const planeShape = new CANNON.Plane();
-const planeShape = new CANNON.Box(new CANNON.Vec3(5, 0.1, 5));
-// 创建一个刚体
-const planeBody = new CANNON.Body({
-  // mass: 0,
-  shape: planeShape,
-  position: new CANNON.Vec3(0, 0, 0),
-  type: CANNON.Body.STATIC,
-});
-planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), 0.1);
 
-// 将刚体添加到物理世界
+//创建一个物理世界的平面
+// const planeShape = new CANNON.Plane();
+const planeShape = new CANNON.Box(new CANNON.Vec3(5,0.1,5));
+const planeBody = new CANNON.Body({
+  // mass:0,
+  shape:planeShape,
+  position:new CANNON.Vec3(0,0,0),
+  type:CANNON.Body.STATIC
+})
+// planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0),-Math.PI/2+0.1);
+//刚体添加到物理世界
 world.addBody(planeBody);
 
-// 创建一个球体几何体
-const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
-// 创建一个球体材质
-const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// 创建一个球体网格
-const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
-// 将网格添加到3D场景
+//创建一个球体几何体
+const sphereGeometry = new THREE.SphereGeometry(0.5,32,32);
+//创建一个球体材质
+const sphereMaterial = new THREE.MeshBasicMaterial({color:0x00ff00});
+
+const sphereMesh = new THREE.Mesh(sphereGeometry,sphereMaterial);
+//将网格添加到3D场景
 scene.add(sphereMesh);
 
-// 创建一个平面几何体
-// const planeGeometry = new THREE.PlaneGeometry(10, 10);
-const planeGeometry = new THREE.BoxGeometry(10, 0.2, 10);
-// 创建一个平面材质
-const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-// 创建一个平面网格
-const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
-// x轴旋转90度
-planeMesh.rotation.x = 0.1;
-// 将网格添加到3D场景
+//创建一个平面几何体
+// const planeGeometry = new THREE.PlaneGeometry(10,10);
+const planeGeometry = new THREE.BoxGeometry(10,0.2,10);
+//创建一个平面材质
+const planeMaterial = new THREE.MeshBasicMaterial({color:0xffff00});
+const planeMesh = new THREE.Mesh(planeGeometry,planeMaterial);
+//旋转
+// planeMesh.rotation.x = -Math.PI/2 + 0.1
+//将网格添加到3D场景
 scene.add(planeMesh);
 
-// 渲染
+//渲染
 let clock = new THREE.Clock();
-function animate() {
+function animate(){
+  // let del
+  //2帧之间的时间
   let delta = clock.getDelta();
-  world.step(1 / 60, delta);
-  // 更新球体网格的位置和旋转
+  world.step(1/60,delta);
+  //物理世界的位置复制过去
   sphereMesh.position.copy(sphereBody.position);
+  //复制角度以及其他；
   sphereMesh.quaternion.copy(sphereBody.quaternion);
-
   controls.update();
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
+  renderer.render(scene,camera);
+  window.requestAnimationFrame(animate);
 }
+animate()
 
-animate();
+
 </script>
 
 <style>
